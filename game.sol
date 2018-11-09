0855a5d69d8b76f84d9005544f7e51aabd7994ea
Kartik Garg <kartik.garg3@gmail.com>
2018-11-09 11:24:25 +0530

    Update game.sol

--- game.sol
+++ game.sol
@@ -6,10 +6,17 @@ contract TicTacToe
     uint[] game_board = new uint[](9);
     uint num_winning_states;
     uint player_tracker;
+    byte[] signs = new byte[](3); //variable to store the game signs
     uint[][]  game_state_wins = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8],[2,4,6]  ];
     constructor()
     {
+        signs[0]="-";
+        signs[1]="X";
+        signs[2]="O";
+    
+    
         p1 = msg.sender;
+        player_tracker=0;
         num_winning_states = 8;
     }
 
@@ -18,8 +25,8 @@ contract TicTacToe
     
     function registerplayer()
     {
-        require(p1 != 0);
-        require(p2 == 0);
+        // require(p1 != 0);
+        // require(p2 == 0);
         p2 = msg.sender;
     }
     
@@ -48,14 +55,41 @@ contract TicTacToe
         {
             return "Winner is 2";
         }
-        require(index > 0);
-        require(index < 9);
-        require(game_board[index] != 0);
+        // require(index > 0);
+        // require(index < 9);
+        // require(game_board[index] != 0);
         game_board[index] = player_tracker + 1;
-        player_tracker = player_tracker - 1; // changing the state bacl
+        player_tracker = 1-player_tracker ; // changing the state bacl
         return "Move has been made" ;
     }
     
-    
+    function return_game_board() returns (string,string)
+    {
+     string memory text="Game in progress";
+     uint Winner = checkWinner();
+     if(Winner==2)
+     {
+         text="winner is 2";
+     }
+     if(Winner==1)
+     {
+        text="winner is 1";
+         
+     }
+     
+     
+     bytes memory displayboard = new bytes(11);
+     bytes(displayboard)[3] = "|";
+     bytes(displayboard)[7] = "|";
+     
+     uint k=0;
+     while(k < 9)
+     {
+        bytes(displayboard)[k + k/3] = signs[game_board[k]];
+        k++;
+         
+     }
+     return (text,string(displayboard));
+    }
     
 }
